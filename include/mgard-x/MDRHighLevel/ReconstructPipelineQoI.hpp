23993f4acffea976dad9cae0a706aedf6ac33db1
LinusLi1037 <wenbo.li@uky.edu>
2025-04-04 15:33:20 -0400

    Portable QoI pipeline

--- include/mgard-x/MDRHighLevel/ReconstructPipelineQoI.hpp
+++ include/mgard-x/MDRHighLevel/ReconstructPipelineQoI.hpp
@@ -6,88 +6,14 @@
 #include <string>
 #include <iostream>
 #include <sstream>
-#include <cuda_runtime.h>
 
 #include "mgard-x/Config/Config.h"
 #include "mgard-x/MDRHighLevel/MDRDataHighLevel.hpp"
 #include "mgard-x/MDRHighLevel/MDRHighLevel.hpp"
-#include "mgard-x/MDRHighLevel/qoi_kernel.hpp"
+#include "mgard-x/MDRHighLevel/QoIKernel.hpp"
 
 namespace mgard_x {
 namespace MDR {
-
-// ====================== Support Utils =========================
-
-inline void pin_memory(void *ptr, SIZE num_bytes, const Config &) {
-  cudaError_t err = cudaHostRegister(ptr, num_bytes, cudaHostRegisterPortable);
-  if (err != cudaSuccess) {
-    std::cerr << "cudaHostRegister failed: " << cudaGetErrorString(err) << "\n";
-    exit(1);
-  }
-}
-
-template <typename T>
-inline SIZE readfile(std::string input_file, T *&in_buff) {
-  FILE *pFile = fopen(input_file.c_str(), "rb");
-  if (pFile == NULL) {
-    std::cerr << "file open error: " << input_file << "\n";
-    exit(1);
-  }
-  fseek(pFile, 0, SEEK_END);
-  size_t lSize = ftell(pFile);
-  rewind(pFile);
-  in_buff = (T *)malloc(lSize);
-  fread(in_buff, 1, lSize, pFile);
-  fclose(pFile);
-  return lSize;
-}
-
-// ====================== read_mdr =========================
-
-inline size_t read_mdr(RefactoredMetadata &refactored_metadata,
-                       RefactoredData &refactored_data,
-                       std::string input, bool initialize_signs,
-                       Config config) {
-
-  size_t size_read = 0;
-  int num_subdomains = refactored_metadata.metadata.size();
-
-  for (int subdomain_id = 0; subdomain_id < num_subdomains; subdomain_id++) {
-    MDRMetadata &metadata = refactored_metadata.metadata[subdomain_id];
-    int num_levels = metadata.level_sizes.size();
-
-    for (int level_idx = 0; level_idx < num_levels; level_idx++) {
-      int loaded = metadata.loaded_level_num_bitplanes[level_idx];
-      int requested = metadata.requested_level_num_bitplanes[level_idx];
-
-      for (int bitplane_idx = loaded; bitplane_idx < requested; bitplane_idx++) {
-        std::string filename = "component_" + std::to_string(subdomain_id) + "_" +
-                               std::to_string(level_idx) + "_" +
-                               std::to_string(bitplane_idx);
-        Byte* &ptr = refactored_data.data[subdomain_id][level_idx][bitplane_idx];
-        SIZE size = readfile(input + "/" + filename, ptr);
-        pin_memory(ptr, size, config);
-        SIZE expected = metadata.level_sizes[level_idx][bitplane_idx];
-        if (size != expected) {
-          std::cerr << "mdr component size mismatch: read " << size
-                    << ", expected " << expected << std::endl;
-          exit(-1);
-        }
-        size_read += size;
-      }
-
-      if (initialize_signs) {
-        SIZE num_elems = metadata.level_num_elems[level_idx];
-        bool* &sign_ptr = refactored_data.level_signs[subdomain_id][level_idx];
-        sign_ptr = (bool*)malloc(sizeof(bool) * num_elems);
-        memset(sign_ptr, 0, sizeof(bool) * num_elems);
-        pin_memory(sign_ptr, sizeof(bool) * num_elems, config);
-      }
-    }
-  }
-
-  return size_read;
-}
 
 template <DIM D, typename T, typename DeviceType, typename ReconstructorType>
 void reconstruct_pipeline_qoi(
@@ -105,6 +31,18 @@ void reconstruct_pipeline_qoi(
   Array<D, T, DeviceType> *device_subdomain_buffer =
       Cache::cache.device_subdomain_buffer;
   MDRData<DeviceType> *mdr_data = Cache::cache.mdr_data;
+
+  Array<D, bool, DeviceType> error_out({256, 256, 256});
+  Array<1, bool, DeviceType> error_final_out({1});
+  Array<1, Byte, DeviceType> workspace;
+
+  for(int i=0; i<2; i++){
+    error_final_out.resize({1}, i);
+    DeviceCollective<DeviceType>::AbsMax( 256 * 256 * 256,
+        SubArray<1, T, DeviceType>(), SubArray<1, T, DeviceType>(),
+        workspace, false, 0);
+  }
+
 
   if (config.mdr_qoi_num_variables != domain_decomposer.num_subdomains()) {
     log::err("QOI mode requires the number of variables to be equal to the "
@@ -187,8 +125,8 @@ void reconstruct_pipeline_qoi(
         // for (auto &metadata : refactored_metadata.metadata) {
         //   metadata.PrintStatus();
         // }
-        size_t size_read = read_mdr(refactored_metadata, refactored_data, "/home/linusli037/Polaris/MGARD/build-cuda-turing/mgard/miniNYX/XYZ", false, config);
-        refactored_metadata.total_size += size_read;
+        // size_t size_read = read_mdr(refactored_metadata, refactored_data, "/home/linusli037/Polaris/MGARD/build-cuda-turing/mgard/miniNYX/XYZ", false, config);
+        // refactored_metadata.total_size += size_read;*****
         // initiate the bitplane transfer for the 1st variable which
         // should coorespond to the next_buffer
         mdr_data[0].CopyFromRefactoredData(
@@ -231,14 +169,27 @@ void reconstruct_pipeline_qoi(
         //     reconstructed_data.qoi_in_progress = false;
         //  }
         //  we set it true for testing only
+        DeviceLauncher<DeviceType>::Execute(
+          mgard_x::data_refactoring::multi_dimension::QoIKernel<D, T, DeviceType>(
+                                            SubArray(device_subdomain_buffer[0]), 
+                                            SubArray(device_subdomain_buffer[1]), 
+                                            SubArray(device_subdomain_buffer[2]), 
+                                            error_out, eb_Vx, eb_Vy, eb_Vz, tol), 
+                                          current_queue);
+        SubArray<1, bool, DeviceType> out_1d({256*256*256}, error_out.data());
+        DeviceCollective<DeviceType>::AbsMax(256*256*256, out_1d, SubArray(error_final_out),
+                                 workspace, true, current_queue);
 
-        reconstructed_data.qoi_in_progress = mgard::MDR::V_TOT_error_estimation<T>(
-                (T *) device_subdomain_buffer[0].data(),
-                (T *) device_subdomain_buffer[1].data(),
-                (T *) device_subdomain_buffer[2].data(),
-                refactored_metadata.metadata[0].num_elements,
-                eb_Vx, eb_Vy, eb_Vz, tol
-              );
+        bool error_final_out_host;
+        MemoryManager<DeviceType>::Copy1D(&error_final_out_host, error_final_out.data(), 1,
+                                          current_queue);
+        DeviceRuntime<DeviceType>::SyncQueue(current_queue);
+        reconstructed_data.qoi_in_progress = error_final_out_host ? true : false;
+        if(reconstructed_data.qoi_in_progress){
+            refactored_metadata.total_size += refactored_metadata.metadata[0].retrieved_size
+                                    + refactored_metadata.metadata[1].retrieved_size
+                                    + refactored_metadata.metadata[2].retrieved_size;
+        }
         // std::cout << "reconstructed_data.qoi_in_progress = " << reconstructed_data.qoi_in_progress << std::endl;   
       }
       
